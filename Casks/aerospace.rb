acdcddf391486c9d21aca55a09609124ce55bc41
Nikita Bobko <git@bobko.xyz>
2023-11-03 00:34:08 +0100

    aerospace: 0.1.0-Alpha -> 0.2.0-Beta

--- Casks/aerospace.rb
+++ Casks/aerospace.rb
@@ -1,6 +1,6 @@
 cask "aerospace" do
-  version "0.1.0-Alpha"
-  sha256 "b6ba11b74d0909ba73a113dab29da67de8d2055ccbb77f8a0c1ba585b6b89d64"
+  version "0.2.0-Beta"
+  sha256 "b3f1294b6b675a998f9b40921d5fa2df46f3483e97d5b9736961acbd3647f2fc"
 
   url "https://github.com/nikitabobko/AeroSpace/releases/download/v#{version}/AeroSpace-v#{version}.zip"
   name "AeroSpace"
