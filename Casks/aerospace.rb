8415d985169e2dbb022ac166d74042829ff068ee
Nikita Bobko <git@bobko.xyz>
2023-11-12 02:57:21 +0100

    Update AeroSpace.app

--- Casks/aerospace.rb
+++ Casks/aerospace.rb
@@ -1,6 +1,6 @@
 cask "aerospace" do
-  version "0.3.0-Beta"
-  sha256 "4b9dc2159a86058141a3bb68806a1b8ee45ae6b59df09cba71bd3ceb0a3ddd4b"
+  version "0.4.0-Beta"
+  sha256 "48d4029222fb9acb12e740c4a90a0abc69e62762cc23c3d45cdfd13d28a1ee5d"
 
   url "https://github.com/nikitabobko/AeroSpace/releases/download/v#{version}/AeroSpace-v#{version}.zip"
   name "AeroSpace"
@@ -9,6 +9,7 @@ cask "aerospace" do
 
   auto_updates true
   depends_on macos: ">= :ventura" # macOS 13
+  depends_on formula: "aerospace-cli"
 
-  app "AeroSpace.app"
+  app "AeroSpace-v#{version}/AeroSpace.app"
 end
