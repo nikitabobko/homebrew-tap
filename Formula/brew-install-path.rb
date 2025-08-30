8d748356717f03a36e9bf9e5847c2ae240f93e0b
Nikita Bobko <git@bobko.xyz>
2025-08-31 00:04:55 +0200

    brew-install-path. version "1.2.0"

--- Formula/brew-install-path.rb
+++ Formula/brew-install-path.rb
@@ -1,9 +1,9 @@
 class BrewInstallPath < Formula
   desc "Simple script that allows installing homebrew formulas/casks from paths"
   homepage "https://github.com/nikitabobko/brew-install-path"
-  url 'https://raw.githubusercontent.com/nikitabobko/brew-install-path/refs/tags/v1.1.0/brew-install-path'
-  version "1.1.0"
-  sha256 "325279456e442d98da4fa6a44fc316fb14786cb2623e6c4295d38b0a30ccbdfe"
+  version "1.2.0"
+  url "https://raw.githubusercontent.com/nikitabobko/brew-install-path/refs/tags/v#{version}/brew-install-path"
+  sha256 "601a56019c66b0d15d582352405ac23392770f9e1d0f4b2e920b10075a4dbfde"
   license "MIT"
   head 'https://github.com/nikitabobko/brew-install-path.git', branch: "main"
 
