7d97e47b25b0b9011f3cf9b4dc9304b77d61b36c
Nikita Bobko <git@bobko.xyz>
2025-08-31 00:09:51 +0200

    Fix GH Actions build
    https://github.com/Homebrew/brew/issues/18371
    https://github.com/Homebrew/brew/pull/20414

--- run-tests.sh
+++ run-tests.sh
@@ -6,12 +6,14 @@ set -o pipefail # Any command failed in the pipe fails the whole pipe
 # cd "$(dirname "$0")"
 cd "$(dirname "$0")"
 
+brew install nikitabobko/tap/brew-install-path
+
 cd ./Casks
 
 for file in *; do
     echo '#############################################################################################################'
     echo "$file"
-    brew install --cask "./$file"
+    brew install-path --cask "./$file"
     version=$(grep '^\s*version' "$file" | head -1 | grep --only-matching -E '[0-9.]+')
     if aerospace -v | grep -q --fixed-strings "$version"; then
         true
